98ee8f9b697d03089cbc58d0cc1e85011cac8718
Mayowa Aj <sagaraaj69@gmail.com>
2019-11-15 02:47:27 +0100

    database seeders

---
+++ database/seeds/PetitionsSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PetitionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('petitions')->insert([
+            'title' => "Petition to *************** to let Mayowa eat her work",
+            'slug' => "petition-to-XXXXX-to-let-mayowa-eat-her-work",
+            'body' => "Help a hungry shoul today",
+            'upvotes' => 100,
+            'downvotes' => 0,
+            'enable_yes' => true,
+            'enable_no' => false
+        ]);
+    }
+}
