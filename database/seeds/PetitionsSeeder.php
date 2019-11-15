ea6ada2197db0409c7c69e25cd57c934e4a667e4
Mayowa Aj <sagaraaj69@gmail.com>
2019-11-15 02:52:19 +0100

    typo

--- database/seeds/PetitionsSeeder.php
+++ database/seeds/PetitionsSeeder.php
@@ -14,7 +14,7 @@ class PetitionsSeeder extends Seeder
         DB::table('petitions')->insert([
             'title' => "Petition to *************** to let Mayowa eat her work",
             'slug' => "petition-to-XXXXX-to-let-mayowa-eat-her-work",
-            'body' => "Help a hungry shoul today",
+            'body' => "Help a hungry soul today",
             'upvotes' => 100,
             'downvotes' => 0,
             'enable_yes' => true,
