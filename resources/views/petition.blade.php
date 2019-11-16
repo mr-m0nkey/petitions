825a228b5ec6db79835fd5ad9a7c53d809ee5be2
Mayowa Aj <sagaraaj69@gmail.com>
2019-11-16 14:24:05 +0100

    commented out nav bar and changed title

--- resources/views/petition.blade.php
+++ resources/views/petition.blade.php
@@ -72,8 +72,8 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        <a class="navbar-brand" href="#">Petitions</a>
+        <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -106,7 +106,7 @@
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
-        </div>
+        </div> -->
     </nav>
 
 
