379178931bc36c5e19905d0d924ed50464dc2022
Mayowa Ajagunna <majagunna@teamapt.com>
2021-01-19 19:05:11 +0100

    formatting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,124 +1,132 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Finding the imposter</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>Finding the imposter</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 20px;
-                padding-top: 80px;
-            }
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Styles -->
+    <style>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 20px;
+            padding-top: 80px;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .full-height {
+            height: 100vh;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        .content {
+            text-align: center;
+        }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        .title {
+            font-size: 84px;
+        }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body >
-        <div class="flex-center position-ref full-height">
-           
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Who is the imposter?
-                </div>
-                <p>2 masks of unknown origin were found in my house today. All 4 suspects have been notified and have been asked to confess but all remain quiet. Now they must rat each other out by voting for who they think owns the masks</p>
-                <p>16:10: One person suspects Ope, Ope denies</p>
-                <p>The person takes back their vote(coward oshi)<p>
-                <p>16:20: Nobody can be trusted, more people cast their votes. Tensions rise</p>
-                <p>
-                <ul>
-                <li>Mide: 1 vote</li>
-                <li>Ore: 1 vote</li>
-                <li>Kayode: 1 vote</li>
-                <li>Mayowa: 1 vote</li>
-        
-                </ul>
-                </p>
+        .m-b-md {
+            margin-bottom: 30px;
+        }
 
-                <p>Accusations everywhere as more people vote
-                <ul>
-                <li>Mide: 2 vots (suspect still dey correct my typo) *votes</li>
-                <li>Ore: 1 vote</li>
-                <li>Kayode: 2 votes</li>
-                <li>Mayowa: 1 vote</li>
-        
-                </ul>
-                </p>
+    </style>
+</head>
+
+<body>
+    <div class="flex-center position-ref full-height">
 
 
-                <p>16:27: One person breaks under pressure and confesses to owning one of the masks. They couldn't deny the evidence provided. But one mask still remains unclaimed</p>
-                <p>The other suspects have been notified of the confession but don't know who confessed, suspense rises.</p>
-
-
-                <p>16:38: Another confession, but the trust has already been broken. Nobody still knows who owns both masks except the moderator. Some have been accused unjustly by those they once considered friends</p>
-                <p>16:48: One person refuses to vote</p>
-                <p>18:58: Someone has claimed the votes of the weyrey who decided not to vote
-                <ul>
-                <li>Mide: 3 votes</li>
-                <li>Ore: 2 votes</li>
-                <li>Kayode: 2 votes</li>
-                <li>Mayowa: 1 vote</li>
-        
-                </ul>
-
-                </p>
-                
-                <p><strong>Find out who the imposter is at 22:00 tonight (19/01/2021)</strong></p>
-
+        <div class="content">
+            <div class="title m-b-md">
+                Who is the imposter?
             </div>
+            <p>2 masks of unknown origin were found in my house today. All 4 suspects have been notified and have been
+                asked to confess but all remain quiet. Now they must rat each other out by voting for who they think
+                owns the masks</p>
+            <p>16:10: One person suspects Ope, Ope denies</p>
+            <p>The person takes back their vote(coward oshi)<p>
+                    <p>16:20: Nobody can be trusted, more people cast their votes. Tensions rise</p>
+                    <p>
+                        <ul>
+                            <li>Mide: 1 vote</li>
+                            <li>Ore: 1 vote</li>
+                            <li>Kayode: 1 vote</li>
+                            <li>Mayowa: 1 vote</li>
+                        </ul>
+                    </p>
 
-      
+                    <p>Accusations everywhere as more people vote
+                        <ul>
+                            <li>Mide: 2 vots (suspect still dey correct my typo) *votes</li>
+                            <li>Ore: 1 vote</li>
+                            <li>Kayode: 2 votes</li>
+                            <li>Mayowa: 1 vote</li>
+                        </ul>
+                    </p>
+
+
+                    <p>16:27: One person breaks under pressure and confesses to owning one of the masks. They couldn't
+                        deny the evidence provided. But one mask still remains unclaimed</p>
+                    <p>The other suspects have been notified of the confession but don't know who confessed, suspense
+                        rises.</p>
+
+
+                    <p>16:38: Another confession, but the trust has already been broken. Nobody still knows who owns
+                        both masks except the moderator. Some have been accused unjustly by those they once considered
+                        friends</p>
+                    <p>16:48: One person refuses to vote</p>
+                    <p>18:58: Someone has claimed the votes of the weyrey who decided not to vote
+                        <ul>
+                            <li>Mide: 3 votes</li>
+                            <li>Ore: 2 votes</li>
+                            <li>Kayode: 2 votes</li>
+                            <li>Mayowa: 1 vote</li>
+                        </ul>
+
+                    </p>
+
+                    <p><strong>Find out who the imposter is at 22:00 tonight (19/01/2021)</strong></p>
+
         </div>
-    </body>
+
+
+    </div>
+</body>
+
 </html>
