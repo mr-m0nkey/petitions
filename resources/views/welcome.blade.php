a3e1b4021615f822737472440c035ed76291bd6c
Mayowa Ajagunna <majagunna@teamapt.com>
2021-01-19 19:21:15 +0100

    edited public voting message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -121,7 +121,7 @@
 
                     </p>
 
-                    <p>19:00: Members of the general public have been given the opportunity to vote.</p>
+                    <p>19:00: Members of the general public have been given the opportunity to vote.(DM your votes)</p>
                     <p>19:11: A vote has been cast against Kayode.
                     <ul>
                             <li>Mide: 3 votes</li>
