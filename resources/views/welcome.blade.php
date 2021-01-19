9a1ba0f4b5d7da7b953829d8db3e3a2806a7c399
Mayowa Ajagunna <majagunna@teamapt.com>
2021-01-19 18:25:53 +0100

    erfrf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,6 +71,19 @@
                 <div class="title m-b-md">
                     Who is the imposter?
                 </div>
+                <p>2 masks of unknown origin were found in my house today. All 4 suspects have been notified and have been asked to confess but all remain quiet. Now they must rat each other out by voting fpor who they think owns the masks</p>
+                <p>16:10: One person suspects Ope, Ope denies</p>
+                <p>The person takes back their vote<p>
+                <p>16:20: Nobody can be trusted, more people cast their votes. Tensions rise</p>
+                <p>
+                <ul>
+                <li>Mide: 1 vote</li>
+                <li>Ore: 1 vote</li>
+                <li>Kayode: 1 vote</li>
+                <li>Mayowa: 1 vote</li>
+        
+                </ul>
+                </p>
                 <p>Find out at 22:00 tonight</p>
 
             </div>
