31c8feca6cf118e20bed161ff8905e64fa1471a6
Mayowa Ajagunna <majagunna@teamapt.com>
2021-01-19 18:38:50 +0100

    dvev

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
-                margin: 0;
+                margin: 20px;
                 padding-top: 80px;
             }
 
